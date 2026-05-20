e1fce846e9426c32e9f5d886cd746391a57c3029
IlariA-16 <ilariataddei@hotmail.com>
2026-05-20 16:11:50 +0200

    Resa dinamica la pagina About con i Signals e strutturato il footer definitivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home} from './pages/home/home';
-import { About } from './pages/about/about';
+import { AboutComponent } from './pages/about/about'; 
 import { Blog } from './pages/blog/blog';
 import { ContactPage } from './pages/contact-page/contact-page';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'about', component: About },
+  { path: 'about', component: AboutComponent },
   { path: 'blog', component: Blog },
   { path: 'contatti', component: ContactPage },
   { path: '**', redirectTo: '' }
--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface TecnologiaSartoriale {
+  titolo: string;
+  descrizione: string;
+  badgeSoftware: string[];
+}
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [], 
   templateUrl: './about.html',
-  styleUrl: './about.css',
+  styleUrl: './about.css'
 })
-export class About {}
+export class AboutComponent {
+  // Gestione dinamica delle tecnologie tramite i Signals di Angular
+  public strumenti = signal<TecnologiaSartoriale[]>([
+    {
+      titolo: 'CAD Industriale',
+      descrizione: 'Sviluppo taglie geometrico, digitalizzazione dei pezzi a sistema, controllo meticoloso dei perimetri e piazzamento industriale automatizzato per ottimizzare i consumi del tessuto.',
+      badgeSoftware: ['Lectra Modaris']
+    },
+    {
+      titolo: 'Tecniche di Atelier',
+      descrizione: 'Costruzione cartamodelli manuali per capispalla strutturati, drappeggi complessi in sbieco, sdifettamento ottico su manichino e stesura di schede tecniche di produzione.',
+      badgeSoftware: ['Moulage', 'Sdifettamento Prototipi']
+    }
+  ]);
+}
